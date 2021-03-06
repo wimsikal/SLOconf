331e338510f47d121b503df485161b7ac15c0ce9
wimsikal <danny@luedkes.com>
2021-03-06 11:57:08 -0800

    feat: pre-conf pages

--- lib/constants.ts
+++ lib/constants.ts
@@ -36,42 +36,58 @@ export const LEGAL_URL = process.env.NEXT_PUBLIC_PRIVACY_POLICY_URL;
 export const COPYRIGHT_HOLDER = process.env.NEXT_PUBLIC_COPYRIGHT_HOLDER;
 
 export const CODE_OF_CONDUCT =
-  'https://www.notion.so/vercel/Code-of-Conduct-Example-7ddd8d0e9c354bb597a0faed87310a78';
+  '/code-of-conduct';
 export const REPO = 'https://github.com/wimsikal/sloconf';
 export const SAMPLE_TICKET_NUMBER = 1234;
 export const NAVIGATION = [
   {
-    name: 'Stage A',
-    route: '/stage/a'
+    name: 'About',
+    route: '/about'
   },
   {
-    name: 'Stage C',
-    route: '/stage/c'
+    name: 'Tracks',
+    route: '/tracks'
   },
   {
-    name: 'Stage M',
-    route: '/stage/m'
+    name: 'Call for Papers',
+    route: '/cfp'
   },
   {
-    name: 'Stage E',
-    route: '/stage/e'
+    name: 'Labs',
+    route: '/labs'
   },
-  {
-    name: 'Schedule',
-    route: '/schedule'
-  },
-  {
-    name: 'Speakers',
-    route: '/speakers'
-  },
-  {
-    name: 'Expo',
-    route: '/expo'
-  },
-  {
-    name: 'Jobs',
-    route: '/jobs'
-  }
+  // {
+  //   name: 'Stage A',
+  //   route: '/stage/a'
+  // },
+  // {
+  //   name: 'Stage C',
+  //   route: '/stage/c'
+  // },
+  // {
+  //   name: 'Stage M',
+  //   route: '/stage/m'
+  // },
+  // {
+  //   name: 'Stage E',
+  //   route: '/stage/e'
+  // },
+  // {
+  //   name: 'Schedule',
+  //   route: '/schedule'
+  // },
+  // {
+  //   name: 'Speakers',
+  //   route: '/speakers'
+  // },
+  // {
+  //   name: 'Expo',
+  //   route: '/expo'
+  // },
+  // {
+  //   name: 'Jobs',
+  //   route: '/jobs'
+  // },
 ];
 
 export type TicketGenerationState = 'default' | 'loading';
