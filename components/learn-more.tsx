071b4c1018231b6971cd7a94214d3a55f01fb8fc
wimsikal <danny@luedkes.com>
2021-03-06 08:55:04 -0800

    feat: learn more

--- components/learn-more.tsx
+++ components/learn-more.tsx
@@ -22,9 +22,11 @@ import { REPO } from '@lib/constants';
 export default function LearnMore() {
   return (
     <div className={cn(styleUtils.appear, styleUtils['appear-fifth'], styles.contact)}>
-      Learn more on{' '}
-      <a href={REPO} className={styles['contact-email']} target="_blank" rel="noopener noreferrer">
-        GitHub
+      <p>Designed for while-you’re-working participation.</p>
+      
+      Learn more about{' '}
+      <a href='/about' className={styles['contact-email']} target="_blank" rel="noopener noreferrer">
+        SLOConf
       </a>
       .
     </div>
